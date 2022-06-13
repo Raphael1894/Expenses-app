4dc4a63bdf95663656319f76837aae30625a8ee5
Raphael1894 <rapha.pereira.perso@gmail.com>
2022-06-13 15:53:56 +0200

    second phase

--- components/expensesOutput/ExpensesList.js
+++ components/expensesOutput/ExpensesList.js
@@ -1,19 +1,20 @@
-import { FlatList, Text } from 'react-native';
+import { FlatList, StyleSheet } from 'react-native';
+import ExpenseItem from './ExpenseItem';
+
+
 
 function renderExpenseItem(itemData) {
-	return <Text>{itemData.item.description}</Text>;
+	return <ExpenseItem {...itemData.item}/>;
 }
 
 function ExpensesList({ expenses }) {
-	return (
-		<FlatList
-			data={expenses}
-			renderItem={renderExpenseItem}
-			keyExtractor={(item) => {
-				item.id;
-			}}
-		/>
-	);
+	return <FlatList data={expenses} renderItem={renderExpenseItem} keyExtractor={(item) => item.id} />;
 }
 
 export default ExpensesList;
+
+const styles = StyleSheet.create({
+	container: {
+		padding: 8
+	}
+});
