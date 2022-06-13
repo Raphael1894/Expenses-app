f35fcc3ef6fa85da1b506d649ec669babea18bab
Raphael1894 <rapha.pereira.perso@gmail.com>
2022-06-13 14:23:15 +0200

    new files - expenses

---
+++ components/expensesOutput/ExpensesSummary.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+
+function ExpensesSummary() {
+	return (
+		<View>
+			<Text>Last 7 days</Text>
+			<Text>99.99€</Text>
+		</View>
+	);
+}
+
+export default ExpensesSummary;
