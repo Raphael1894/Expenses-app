eb70446986a38dc7a090b8b7d09af18633d6985b
Raphael1894 <rapha.pereira.perso@gmail.com>
2022-06-13 14:43:31 +0200

    dummy data & first stage

--- components/expensesOutput/ExpensesOutput.js
+++ components/expensesOutput/ExpensesOutput.js
@@ -3,11 +3,44 @@ import { View } from 'react-native';
 import ExpensesList from './ExpensesList';
 import ExpensesSummary from './ExpensesSummary';
 
-function ExpensesOutput({ expenses }) {
+const DUMMY_EXPENSES = [
+	{
+		id: 'e1',
+		description: 'Pair of shoes',
+		amount: 59.99,
+    date: new Date('2021-12-19')
+	},
+  {
+		id: 'e2',
+		description: 'Pair of shorts',
+		amount: 29.99,
+    date: new Date('2021-11-11')
+	},
+  {
+		id: 'e3',
+		description: 'Bananas',
+		amount: 5.99,
+    date: new Date('2021-06-05')
+	},
+  {
+		id: 'e4',
+		description: 'Book',
+		amount: 15.99,
+    date: new Date('2021-02-05')
+	},
+  {
+		id: 'e5',
+		description: 'Cellphone',
+		amount: 415.99,
+    date: new Date('2021-12-17')
+	}
+];
+
+function ExpensesOutput({ expenses, expensesPeriod }) {
 	return (
 		<View>
-			<ExpensesSummary />
-			<ExpensesList />
+			<ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod}  />
+			<ExpensesList expenses={DUMMY_EXPENSES}/>
 		</View>
 	);
 }
--- components/expensesOutput/ExpensesSummary.js
+++ components/expensesOutput/ExpensesSummary.js
@@ -1,10 +1,14 @@
 import { View, Text } from 'react-native';
 
-function ExpensesSummary() {
+function ExpensesSummary({ periodName, expenses }) {
+	const expensesSum = expenses.reduce((sum, expense) => {
+		return sum + expense.amount;
+	}, 0);
+
 	return (
 		<View>
-			<Text>Last 7 days</Text>
-			<Text>99.99€</Text>
+			<Text>{periodName}</Text>
+			<Text>{expensesSum.toFixed(2)}€</Text>
 		</View>
 	);
 }
